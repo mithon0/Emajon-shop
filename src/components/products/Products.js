13c694d27bb97f91ceed2da6ce7fde8cbbd8be4b
mithon0 <mithonhossen7244@gmail.com>
2023-03-28 17:01:24 +0600

    local storage

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,44 +1,64 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../../cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 
 import Product from '../product/Product';
 import './Products.css'
 const Products = () => {
-    const [shopData,setShopDAta]=useState([]);
-    const [cart,setCart]=useState([]);
+    const [shopData, setShopDAta] = useState([]);
+    const [cart, setCart] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('products.json')
-        .then(res=>res.json())
-        .then(data=>setShopDAta(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setShopDAta(data))
+    }, [])
 
-// addTocart handlar
-const addToCart =(product)=>{
-    // console.log("clicked" ,product);
-    const newCart=[...cart,product]
-    setCart(newCart);
-}
+    // local storage effect 
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        // console.log(storedCart);
+        const savedCart =[];
+        for(const id in storedCart){
+            
+           const addedProduct =shopData.find(product => product.id === id)
+           
+           if(addedProduct){
+            const quantity =addedProduct.quantity;
+            storedCart[id]=quantity;
+            savedCart.push(addedProduct);
+           }
+        }
+        setCart(savedCart);
+
+    }, [shopData])
+    // addTocart handlar
+    const addToCart = (product) => {
+        // console.log("clicked" ,product);
+        const newCart = [...cart, product]
+        setCart(newCart);
+        addToDb(product.id)
+    }
 
     return (
         <div className='product-container'>
-           <div className='card-container'>
-           <div className='card'>
-           {
-            shopData.map(shope=><Product
-            key={shope.id}
-            data={shope}
-            addToCart={addToCart}
-            ></Product>)
-           }
-           </div>
-           </div>
-          <div className='cart-container'>
-         <Cart
-         cart={cart}
-         ></Cart>
-          </div>
+            <div className='card-container'>
+                <div className='card'>
+                    {
+                        shopData.map(shope => <Product
+                            key={shope.id}
+                            data={shope}
+                            addToCart={addToCart}
+                        ></Product>)
+                    }
+                </div>
+            </div>
+            <div className='cart-container'>
+                <Cart
+                    cart={cart}
+                ></Cart>
+            </div>
         </div>
     );
 };
